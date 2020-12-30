1ceedd8f7c7466c521a70fc7b556451904087b32
Atemkln <adambeach16@gmail.com>
2020-12-30 17:59:00 -0500

    a

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    private Button button;
+    private GameManager gameManager;
+    public bool ison;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        button.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameManager.StartGame();
+    }
+}
