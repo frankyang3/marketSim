09c42d71ba8053477907b330aef8bbf766347e87
Atemkln <adambeach16@gmail.com>
2020-12-31 16:38:53 -0500

    a

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -7,7 +7,6 @@ public class StartButton : MonoBehaviour
 {
     private Button button;
     private GameManager gameManager;
-    public GameObject titleScreen;
 
     // Start is called before the first frame update
     void Start()
