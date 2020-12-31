9d42a4f44d858ac4aa59dd23893dba1f7a5fea4d
Simoidne <simmy.wu165@gmail.com>
2020-12-31 17:46:59 -0500

    fixed dumb button text bug

--- Assets/Scripts/Simond's Trading Scripts/BuyButton.cs
+++ Assets/Scripts/Simond's Trading Scripts/BuyButton.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuyButton : MonoBehaviour
 {
-    public int buyAmount = 1; // later use get component of the trader and change to private
-    public int itemAvailable = 4;
+    private int buyAmount = 1; // later use get component of the trader and change to private
+    private int itemAvailable = 4; // later take the items available from the trader and change to private
+    public Text buyText;
+    // public string itemName;
 
     private void Start()
+    {
+        GameObject.Find("Buy Button").GetComponentInChildren<Text>().text = "Buy for $" + buyAmount + "  (" + itemAvailable + " left)";
+    }
+
+    private void Update()
     {
         
     }
 
     public void reducePlayerGold()
     {
-        PlayerController.playerGold -= buyAmount;
+        if (itemAvailable > 0)
+        {
+            PlayerController.playerGold -= buyAmount;
+            itemAvailable--;
+            Debug.Log("this is itemAvaiable: " + itemAvailable);
+            Debug.Log("this is buyText: ", buyText);
+            GameObject.Find("Buy Button").GetComponentInChildren<Text>().text = "Buy for $" + buyAmount + "  (" + itemAvailable + " left)";
+        }
     }
 }
