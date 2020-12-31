65b05e0b56325aec6f80acde8520ebdef7b73c68
frankyang3 <frankyang3@gmail.com>
2020-12-31 14:48:43 -0500

    franky Changes to movement

--- Assets/Scripts/moveFwd.cs
+++ Assets/Scripts/moveFwd.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class moveFwd : MonoBehaviour
 {
+    private float speed = 15;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
     }
 }
