c6cb2ddfae147a30e9a5ee695f17248bcb166539
Atemkln <adambeach16@gmail.com>
2021-01-03 15:37:46 -0500

    a

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,6 +22,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         tradeWindow.SetActive(false);
+        timerText.
     }
 
     public void StartGame()
@@ -67,7 +68,10 @@ public class GameManager : MonoBehaviour
 
     public void UpdateTimer(int timeRemaining)
     {
-        timerText.text = "Time: " + timeRemaining;
+        if (isGameActive)
+        {
+            timerText.text = "Time: " + timeRemaining;
+        }
     }
 
     public void GameOver()
